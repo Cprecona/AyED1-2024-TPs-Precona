fa991f25cbcc5cee6ce0ba85fcd1f29641b81ff5
Cprecona <cprecona@uade.edu.ar>
2024-08-25 22:15:00 -0300

    Mejora de ejercicio 2.4

--- TP02/Ejercicio 2.4.py
+++ TP02/Ejercicio 2.4.py
@@ -1,14 +1,20 @@
 def borrar_duplicados(l1,l2):
     '''Recibe dos listas y elimina en la primera lista los elementos que figuran repetidos respecto de la segunda lista.
-    Pre: pasar como argumentos dos listas.
-    Pos: devuelve la primera lista modificada.
+        Pre: pasar como argumentos dos listas.
+    Pos: imprime las dos listas originales, la lista con los elementos eliminados y la original modificada.
     '''
+    print(f"Lista original sin modificaciones ->{l1}")
+    print(f"Segunda lista ->{l2}")
+    lista_elim =[]
     for elem in l1:
         if elem in l2:
             l1.remove(elem)
-    return l1
+            lista_elim.append(elem)
+    print(f"Lista de elementos elminados de la primera lista ->{lista_elim}")
+    print(f"Lista de original con las eliminaciones ->{l1}")
 
+# Ejemplos de listas para la impresión
 lista1=[3,4,5,6,7,8]
 lista2=[1,2,3,4,5,6]
 
-print(borrar_duplicados(lista1,lista2))
+borrar_duplicados(lista1,lista2)
