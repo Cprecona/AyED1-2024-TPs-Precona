728cc63ba6b4803eea60708b2d1283ab1fb74cb7
Cprecona <cprecona@uade.edu.ar>
2024-09-27 13:37:39 -0300

    Modificado Ejercicio 3.2.py

--- TP03/Ejercicio 3.2.py
+++ TP03/Ejercicio 3.2.py
@@ -122,85 +122,6 @@ def cargar_matriz_f(matriz: list[list]) -> list[list]:
            
     return matriz
 
-def cargar_matriz_g(matriz: list[list]) -> list[list]:
-    '''Recibe una matriz y la carga con números de la siguiente manera:
-    Comenzando por el 1, carga números consecutivos, recorriendo la matriz
-    de manera espiral. Recorre la primera fila, luego la última columna,
-    luego la última fila, la primera columna y así sucesivamente. 
-
-    Pre: ingresar una matriz.
-
-    Post: devuelve la matriz cargada.
-    '''
-    n = len(matriz)
-    m = len(matriz[0])
-    primer_numero = 1
-    tope, fondo = 0, n - 1
-    izquierda, derecha = 0, m - 1
-
-    while tope <= fondo and izquierda <= derecha:
-        for j in range(izquierda, derecha + 1):
-            matriz[tope][j] = primer_numero
-            primer_numero += 1
-        tope += 1
-
-        for i in range(tope, fondo + 1):
-            matriz[i][derecha] = primer_numero
-            primer_numero += 1
-        derecha -= 1
-
-        if tope <= fondo:
-            for j in range(derecha, izquierda - 1, -1):
-                matriz[fondo][j] = primer_numero
-                primer_numero += 1
-            fondo -= 1
-
-        if izquierda <= derecha:
-            for i in range(fondo, tope - 1, -1):
-                matriz[i][izquierda] = primer_numero
-                primer_numero += 1
-            izquierda += 1
-
-    return matriz
-
-def cargar_matriz_h(matriz: list[list]) -> list[list]:
-    '''Recibe una matriz y la carga con números de la siguiente manera:
-    Comenzando por el 1, carga números consecutivos, recorriendo la matriz
-    de manera diagonal invirtiendo el sentido de dirección.
-
-    Pre: ingresar una matriz.
-
-    Post: devuelve la matriz cargada.
-    '''
-    n = len(matriz)
-    if n == 0:
-        return matriz
-
-    m = len(matriz[0])
-    primer_numero = 1
-
-    for d in range(n + m - 1):
-        if d % 2 == 0:  
-            col_comienzo = min(d, m - 1)
-            row_comienzo = d - col_comienzo
-            while col_comienzo >= 0 and row_comienzo < n:
-                matriz[row_comienzo][col_comienzo] = primer_numero
-                primer_numero += 1
-                col_comienzo -= 1
-                row_comienzo += 1
-        else:
-            row_comienzo = min(d, n - 1)
-            col_comienzo = d - row_comienzo
-            while row_comienzo >= 0 and col_comienzo < m:
-                matriz[row_comienzo][col_comienzo] = primer_numero
-                primer_numero += 1
-                row_comienzo -= 1
-                col_comienzo += 1
-
-    return matriz
-
-
-
 # PROGRAMA
 
 matriz_prueba = crear_matriz()
@@ -233,12 +154,4 @@ matriz_f = cargar_matriz_f(matriz_prueba_6)
 for fila in matriz_f:
     print(fila)
 
-matriz_prueba_7 = crear_matriz()
-matriz_g = cargar_matriz_g(matriz_prueba_7)
-for fila in matriz_g:
-    print(fila)
-
-matriz_prueba_8 = crear_matriz()
-matriz_h = cargar_matriz_h(matriz_prueba_8)
-for fila in matriz_h:
-    print(fila)
+# FALTA DESARROLLAR LAS ÚLTIMAS 2
