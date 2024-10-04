b6449568e24ef392b4e3d21c3fee7e650f442948
Cprecona <cprecona@uade.edu.ar>
2024-10-04 09:09:46 -0300

    Agregado ejercicio 4.6

---
+++ TP04/Ejercicio 4.6.py
@@ -0,0 +1,46 @@
+def extraer_subcadena_rebanadas(cadena: str,pos: int, cant: int) -> str:
+    '''Recibe una cadena de caracteres y extrae de la misma
+    una subcadena cuya posición y cantidad de caracteres deseada
+    es pasada como parámetro. Devuelve la cadena como retorno.
+
+    Pre: la cadena debe ser un string. La posición y cantidad se pasan
+    como parámetros y deben ser enteros.
+
+    Post: retorna una subcadena como string.
+    '''
+    subcadena = cadena[pos:pos+cant:]
+
+    return subcadena
+
+def extraer_iterando_(cadena: str,pos: int, cant: int) -> str:
+    '''Recibe una cadena de caracteres y extrae de la misma
+    una subcadena cuya posición y cantidad de caracteres deseada
+    es pasada como parámetro. Devuelve la cadena como retorno.
+
+    Pre: la cadena debe ser un string. La posición y cantidad se pasan
+    como parámetros y deben ser enteros.
+
+    Post: retorna una subcadena como string.
+    '''
+    posicion_inicio = pos
+    subcadena_iterando = ""
+    while len(subcadena_iterando) < cant:
+        for posicion, caracter in enumerate(cadena):
+            if posicion >= posicion_inicio:
+                subcadena_iterando += caracter
+
+    return subcadena
+
+# PROGRAMA
+
+# Extracción subcadena utilizando rebanadas
+
+cadena_original = "El número de teléfono es 4356-7890"
+subcadena = extraer_subcadena_rebanadas(cadena_original,25,9)
+print(subcadena)
+
+# Extracción subcadena sin utilizar rebanadas
+
+cadena_original = "El número de teléfono es 4356-7890"
+subcadena_2 = extraer_iterando_(cadena_original,25,9)
+print(subcadena_2)
