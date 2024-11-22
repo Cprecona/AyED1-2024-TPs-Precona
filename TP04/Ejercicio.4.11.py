32958d87e410770ac18321a839620318807f8605
Cprecona <cprecona@uade.edu.ar>
2024-11-21 23:25:15 -0300

    Agregado ejercicio 4.11 que no dio

---
+++ TP04/Ejercicio.4.11.py
@@ -0,0 +1,21 @@
+# PROGRAMA CONTADOR DE SUBCADENAS
+
+'''Muestra la cantidad de veces que se repite una subcadena en una cadena.
+Los caracteres no deben estar necesariamente juntos pero sí aparecer en orden.
+
+Pre: la cadena y subcadena son strings.
+
+Post: imprime la cantidad de veces que aparece.
+'''
+
+subcadena = "UADE"
+cadena = "Platero es pequeño, peludo, suave; tan blando por fuera, que se diría todo de algodón, que no lleva huesos. Sólo los espejos de azabache de sus ojos son duros cual dos escarabajos de cristal negro."
+
+for letra in subcadena:
+    posicion = cadena.find(letra)
+   
+        
+
+print(posicion)
+
+# ESTE NO SALIÓ
