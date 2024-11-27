2fb00614978d924eda54a1bdbf435df5a50f8856
Cprecona <cprecona@uade.edu.ar>
2024-11-27 20:10:04 -0300

    Agregado ejercicio 7.2

---
+++ TP07/Ejercicio.7.2.py
@@ -0,0 +1,31 @@
+import sys
+
+sys.setrecursionlimit(2000)
+
+
+def convertir_a_decimal(num: int, potencia: int, resultado: int) -> int:
+    """Desarrollar una función que reciba un número binario y lo devuelva convertido a
+    base decimal.
+
+    """
+    if num == 0:
+        return resultado
+    elif num % 10 == 0:
+        resultado += 0 * 2**potencia
+        return convertir_a_decimal(num // 10, potencia + 1, resultado)
+    else:
+        resultado += 1 * 2**potencia
+        return convertir_a_decimal(num // 10, potencia + 1, resultado)
+
+
+def main() -> None:
+    """Este es el programa principal. Solamente llama a la funcion
+    contar_digitos y le pasa el número como parámetro.
+
+    """
+    numero_convertido = convertir_a_decimal(10101100, 0, 0)
+    print(numero_convertido)
+
+
+if __name__ == "__main__":
+    main()
